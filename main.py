a54bfc3ca94fd8115d5848122c68c56607ef1c8b
Yutong chen <yuton.chen@mail.utoronto.ca>
2020-12-19 09:32:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,23 +20,23 @@ def predict(x):
     label = classes[torch.argmax(scaled).item()]
     label_num = torch.argmax(scaled).item()
     return {'label': label, 'probability': prob, 'index': label_num}
-    
-    
+
+
 def batch(size):
     npzfile = np.load("./read_images/raf_db.npz")
     npzfile1 = np.load("./read_images/toronto_face.npz")
-    
+
     train_images = npzfile["inputs_train"]
     train_labels = np.argmax(npzfile["target_train"], axis=1)
     test_images = npzfile["inputs_valid"]
     test_labels = np.argmax(npzfile["target_valid"], axis=1)
-    
+
     train_images1 = npzfile1["inputs_train"]
     train_labels1 = npzfile1["target_train"]
     test_images1 = npzfile1["inputs_valid"]
     test_labels1 = npzfile1["target_valid"]
-    
-    
+
+
     for i in range(len(train_labels1)):
         if train_labels1[i] == 0:
             train_labels1[i] = 5
@@ -54,7 +54,7 @@ def batch(size):
             train_labels1[i] = 6
         else:
             print("wrong train label",train_labels1[i])
-        
+
     for i in range(len(test_labels1)):
         if test_labels1[i] == 0:
             test_labels1[i] = 5
@@ -72,12 +72,12 @@ def batch(size):
             test_labels1[i] = 6
         else:
             print("wrong test label",test_labels1[i])
-    
+
     train_images = np.concatenate((train_images, train_images1))
     train_labels = np.concatenate((train_labels, train_labels1))
     test_images = np.concatenate((test_images, test_images1))
     test_labels = np.concatenate((test_labels, test_labels1))
-    
+
     random_permutation = np.random.permutation(train_images.shape[1])
     for i in range(size):
         pyplot.subplot(4, 30, i+1)
@@ -85,15 +85,15 @@ def batch(size):
         pyplot.axis('off')
     pyplot.savefig("test.png", bbox_inches='tight')
     pyplot.show()
-    
-    
+
+
 if __name__ == "__main__":
     #batch(120)
     model = Model(1, 7)
     softmax = torch.nn.Softmax(dim=1)
     model.load_state_dict(torch.load('9.pth', map_location=get_default_device()))
-    out, faces = FaceRec("test2.JPG")
-    image = cv2.imread("test2.JPG")
+    out, faces = FaceRec("test2.jpg")
+    image = cv2.imread("test2.jpg")
     for i, face in zip(out, faces):
         img = torch.from_numpy(i.reshape((48, 48)))
         img = imgTensor(img)
